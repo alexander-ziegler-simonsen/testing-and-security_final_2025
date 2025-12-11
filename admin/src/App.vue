4e2f4f167ceab097dbbcef04c6a54f96f084a86e
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-12-11 14:15:22 +0100

    client - added the same water.css stylesheet, that was added on admin

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,12 +1,8 @@
 <script setup lang="ts">
-import NavBar from './components/NavBar.vue'
+  import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
   <NavBar />
   <RouterView />
-  <!-- <div>
-    <p>this is App</p>
-  </div>
-  <Counter msg="Vite + Vue" /> -->
 </template>
