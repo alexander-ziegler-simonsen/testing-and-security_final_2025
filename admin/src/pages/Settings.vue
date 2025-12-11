e86fc30ad01d1ad37dd97e4d6bdd3750181a63a4
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-12-11 01:21:47 +0100

    admin - init of the first pages

---
+++ admin/src/pages/Settings.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Settings</h1>
+    <p>text here 4</p>
+</template>
---
+++ admin/src/pages/Users.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h1>Users</h1>
+    <p>text here 3</p>
+</template>
