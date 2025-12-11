dfe5414e4adde8509d039fad531304a852a71711
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-12-11 14:14:37 +0100

    client - changed eslint a little, did some 'lint' cleanup, removed some packages

--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,4 +1,3 @@
-<!-- src/pages/HomePage.vue -->
 <template>
   <div class="p-6">
     <h1 class="text-3xl font-bold mb-4">
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,13 +1,7 @@
-// vite.config.ts
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import eslint from "vite-plugin-eslint"
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/tests/setup.ts'
-  }
+  plugins: [vue()],
 })
