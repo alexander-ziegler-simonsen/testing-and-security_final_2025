dadff0745bdaf8c0643d6d5be7722100b28adb32
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-12-13 02:54:13 +0100

    api - cleanup

--- api/src/db/prisma.ts
+++
@@ -1,4 +0,0 @@
-// src/db/prisma.ts
-import { PrismaClient } from "../generated/prisma/client.js"; // adjust path if different
-
-export const prisma = new PrismaClient();
--- api/src/modules/product/product.schema.ts
+++
@@ -1,9 +0,0 @@
-// src/modules/product/product.schema.ts
-import { z } from "zod";
-
-export const createProductSchema = z.object({
-    name: z.string().min(2),
-    price: z.number().positive(),
-});
-
-export const updateProductSchema = createProductSchema.partial();
