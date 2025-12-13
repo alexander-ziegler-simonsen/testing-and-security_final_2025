2a152b93038dfb52e6b26c3608c17f4748db6583
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-12-13 03:03:07 +0100

    client - more paths

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,10 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePage from '../pages/HomePage.vue'
 import About from '../pages/AboutPage.vue'
+import Login from '../pages/LoginPage.vue'
+import NewUser from '../pages/CreateUserPage.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: HomePage },
-    { path: '/about', name: 'About', component: About }
+    { path: '/about', name: 'About', component: About },
+    { path: '/login', name: 'Login', component: Login },
+    { path: '/create_user', name: 'New user', component: NewUser }
 ]
 
 const router = createRouter({
