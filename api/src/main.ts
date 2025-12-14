2dc91ee3996d9da853f26977428de080e7a2e8ec
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-12-14 02:09:26 +0100

    api - added the routes to main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,6 +1,8 @@
 import dotenv from 'dotenv'
 
 import userRoutes from './routes/UserRoutes';
+import productRoutes from './routes/ProductRoutes';
+import commentRoutes from './routes/CommentRoutes';
 
 import express from 'express';
 dotenv.config({ path: '.env' });
@@ -10,7 +12,8 @@ const port = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use("/api/users", userRoutes);
-
+app.use("/api/products", productRoutes);
+app.use("/api/comments", commentRoutes);
 
 
 app.get('/', (req, res) => {
