4031d612739b4686690b715b6327d126bef5a37c
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-12-16 17:25:06 +0100

    client - added a logoutButton component and added to the navbar

---
+++ client/src/components/LogoutButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useAuthStore } from '../store/auth';
+
+const auth = useAuthStore();
+
+function logout() {
+    auth.clearAuth();
+}
+</script>
+
+<template>
+    <button @click="logout">
+        Logout
+    </button>
+</template>
