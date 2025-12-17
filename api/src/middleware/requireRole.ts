cc32c03e707abae03c8a15d7a6484906e8347142
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-12-17 15:51:01 +0100

    api - added a 'requireRole' to middleware

---
+++ api/src/middleware/requireRole.ts
@@ -0,0 +1,16 @@
+import { Response, NextFunction } from "express";
+import { AuthRequest } from "./JwtGuard";
+
+export const requireRole = (role: "user" | "admin") => {
+    return (req: AuthRequest, res: Response, next: NextFunction) => {
+        if (!req.user) {
+            return res.status(401).json({ message: "Not authenticated" });
+        }
+
+        if (req.user.role !== role) {
+            return res.status(403).json({ message: "Forbidden" });
+        }
+
+        next();
+    };
+};
