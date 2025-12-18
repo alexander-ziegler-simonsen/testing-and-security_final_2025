c014f561f3efa829777873411f87c41cb87adb61
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2025-12-18 01:23:56 +0100

    api - more endpoints and static files allow for images

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,18 +5,39 @@ import userRoutes from './routes/UserRoutes';
 import productRoutes from './routes/ProductRoutes';
 import commentRoutes from './routes/CommentRoutes';
 import AuthRoutes from './routes/AuthRoutes';
+import searchRoutes from "./routes/SearchRoutes";
 
 import express from 'express';
+import imageRoutes from "./routes/ImageRoutes";
 //dotenv.config({ path: '.env' });
+
+import path from "path";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+// image requests
+app.use("/uploads", 
+    express.static(path.join(process.cwd(), 
+    "src/uploads"), {
+        immutable: true,
+        maxAge: "30d"
+    })
+);
+
+// this one have to be before express.json
+app.use("/api/images", imageRoutes);
+
 app.use(express.json());
+
 app.use("/api/users", userRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/comments", commentRoutes);
 app.use("/auth", AuthRoutes);
+app.use("/api/search", searchRoutes);
+
+
+
 
 
 app.get('/', (req, res) => {
