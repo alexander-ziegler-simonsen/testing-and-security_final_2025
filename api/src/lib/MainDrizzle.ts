f9a737f4ed908a6f7558dee03e36b874257fd5de
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-01-21 22:31:06 +0100

    added the drizzle config and MainDrizzle

---
+++ api/src/lib/MainDrizzle.ts
@@ -0,0 +1,13 @@
+// Make sure to install the 'pg' package 
+import 'dotenv/config';
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "../db/schema";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+export const db = drizzle( pool , {
+    schema,
+    logger: true,
+});
