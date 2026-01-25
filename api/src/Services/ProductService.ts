e78c500e9977e14c69227025b8114903848cd257
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-01-25 18:57:03 +0100

    api - replaced all the Prisma Product CRUD logic with Drizzle

--- api/src/Services/ProductService.ts
+++ api/src/Services/ProductService.ts
@@ -1,49 +1,64 @@
-import { Prisma } from "../generated/prisma/client";
-import { MainPrisma } from "../lib/MainPrisma";
+//import { Prisma } from "../generated/prisma/client";
+//import { MainPrisma } from "../lib/MainPrisma";
 import { ProductCreateSchema, ProductResponseSchema, ProductCreateDTO, ProductUpdateDTO } from "../schemas/ProductSchema";
+import { db } from "../lib/MainDrizzle";
+import { products} from "../db/schema";
+import { eq } from "drizzle-orm";
+import { createInsertSchema, createSelectSchema, createUpdateSchema } from 'drizzle-zod';
+
+const selectSchema = createSelectSchema(products);
+const selectIdSchema = createSelectSchema(products).pick({id: true});
+const addSchema = createInsertSchema(products);
+const updateSchema = createUpdateSchema(products);
 
 export const createProduct = async (input: ProductCreateDTO) => {
-    const newProduct = await MainPrisma.products.create({data: input});
-    return ProductCreateSchema.parse(newProduct);
+    const newProduct = await addSchema.parse(input);
+    return await db.insert(products).values(newProduct);
 };
 
 export const getProducts = async () => {
-    const Products = await MainPrisma.products.findMany();
-    return Products.map((pro) =>  ProductResponseSchema.parse(pro));
+    const Products = await db.select().from(products);
+    return selectSchema.array().parse(Products);
 };
 
 export const getProductById = async (id: number) => {
-    const Product = await MainPrisma.products.findUnique({ where: { id } });
-    return Product ? ProductResponseSchema.parse(Product) : null;
+    const Product = selectIdSchema.parse(id);
+    const oneProduct = await db.select().from(products).where(eq(products.id, Product.id));
+    return selectSchema.parse(oneProduct);
 };
 
 export const updateProduct = async (id: number, data: ProductUpdateDTO) => {
-    const updatedProduct = await MainPrisma.products.update({ where: { id }, data });
-    return ProductResponseSchema.parse(updatedProduct)
+    const updatedProduct = updateSchema.parse(data);
+    const newProdut = await db.update(products).set(updatedProduct).where(eq(products.id, id))
+    return selectSchema.parse(newProdut);
 };
 
 export const deleteProduct = async (id: number) => {
     // const deletedProduct = await MainPrisma.products.delete({ where: { id } });
     // return ProductResponseSchema.parse(deletedProduct);
 
-    try {
-        MainPrisma.$transaction(async (tx) => {
-        // delete everything that is connected to this post
-        await tx.comments.deleteMany({where: {fk_product_id: id} });
-        await tx.productImages.deleteMany({where: {fk_product_id: id} });
-        await tx.productFavorite.deleteMany({where: {fk_product_id: id} });
+    const deletedProduct = selectIdSchema.parse(id);
+    const result = await db.delete(products).where(eq(products.id, deletedProduct.id));
+    return result;
 
-        // then delete the post
-        await tx.products.delete({where: {id} });
-        });
+    // try {
+    //     MainPrisma.$transaction(async (tx) => {
+    //     // delete everything that is connected to this post
+    //     await tx.comments.deleteMany({where: {fk_product_id: id} });
+    //     await tx.productImages.deleteMany({where: {fk_product_id: id} });
+    //     await tx.productFavorite.deleteMany({where: {fk_product_id: id} });
 
-        return true;
-    } catch (err) {
-        if (
-            err instanceof Prisma.PrismaClientKnownRequestError && err.code === "P2025"
-        ) {
-            return false;
-        }
-        throw err;
-    }
+    //     // then delete the post
+    //     await tx.products.delete({where: {id} });
+    //     });
+
+    //     return true;
+    // } catch (err) {
+    //     if (
+    //         err instanceof Prisma.PrismaClientKnownRequestError && err.code === "P2025"
+    //     ) {
+    //         return false;
+    //     }
+    //     throw err;
+    // }
 }
