fab0cd4eea8d539f060a7c2724fcd6db1883f8cd
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-01-25 18:56:40 +0100

    api - replaced all the Prisma users CRUD logic with Drizzle

--- api/src/Services/UserService.ts
+++ api/src/Services/UserService.ts
@@ -1,41 +1,54 @@
-import { Prisma } from "../generated/prisma/client";
-import { MainPrisma } from "../lib/MainPrisma";
-import {
-    UserResponseSchema,
-    UserCreateDTO, UserUpdateDTO
-} from "../schemas/UserSchema";
+// import { Prisma } from "../generated/prisma/client";
+// import { MainPrisma } from "../lib/MainPrisma";
+
+import { db } from "../lib/MainDrizzle";
+import { users } from "../db/schema";
+import { eq } from "drizzle-orm";
+import { createInsertSchema, createSelectSchema, createUpdateSchema } from 'drizzle-zod';
+import { UserResponseSchema, UserCreateDTO, UserUpdateDTO } from "../schemas/UserSchema";
+
+const selectSchema = createSelectSchema(users);
+const selectIdSchema = createSelectSchema(users).pick({id: true});
+const addSchema = createInsertSchema(users);
+const updateSchema = createUpdateSchema(users);
 
 export const createUser = async (input: UserCreateDTO) => {
-    try {
-        const newUser = await MainPrisma.users.create({ data: input });
-        return UserResponseSchema.parse(newUser);
-    } catch (err) {
-        if (
-            err instanceof Prisma.PrismaClientKnownRequestError &&
-            err.code === "P2002"
-        ) {
-            return null;
-        }
-        throw err;
-    }
+    const newUser = selectSchema.parse(input);
+    return await db.insert(users).values(newUser);
+    
+    // try {
+    //     const newUser = await MainPrisma.users.create({ data: input });
+    //     return UserResponseSchema.parse(newUser);
+    // } catch (err) {
+    //     if (
+    //         err instanceof Prisma.PrismaClientKnownRequestError &&
+    //         err.code === "P2002"
+    //     ) {
+    //         return null;
+    //     }
+    //     throw err;
+    // }
 };
 
 export const getUsers = async () => {
-    const users = await MainPrisma.users.findMany();
-    return users.map((u) => UserResponseSchema.parse(u));
+    const Users = await db.select().from(users);
+    return selectSchema.array().parse(Users);
 };
 
 export const getUserById = async (id: number) => {
-    const user = await MainPrisma.users.findUnique({ where: { id } });
-    return user ? UserResponseSchema.parse(user) : null;
+    const userId = selectIdSchema.parse(id);
+    const oneUser = await db.select().from(users).where(eq(users.id, userId.id));
+    return selectSchema.parse(oneUser);
 };
 
 export const updateUser = async (id: number, data: UserUpdateDTO) => {
-    const updatedUser = await MainPrisma.users.update({ where: { id }, data });
-    return UserResponseSchema.parse(updatedUser)
+    const updatedUser = updateSchema.parse(data);
+    const newUser = await db.update(users).set(updatedUser).where(eq(users.id, id));
+    return selectSchema.parse(newUser);
 };
 
 export const deleteUser = async (id: number) => {
-    const deletedUser = await MainPrisma.users.delete({ where: { id } });
-    return UserResponseSchema.parse(deletedUser);
+    const deletedUser = selectIdSchema.parse(id);
+    const result = await db.delete(users).where(eq(users.id, deletedUser.id));
+    return result;
 }
