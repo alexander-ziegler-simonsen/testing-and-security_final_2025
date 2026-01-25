c272cc147c0df7bbcf0d2301f1714ae7484faf85
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-01-25 18:57:14 +0100

    api - replaced all the Prisma Auth logic with Drizzle

--- api/src/Services/AuthService.ts
+++ api/src/Services/AuthService.ts
@@ -1,10 +1,12 @@
-import { MainPrisma } from "../lib/MainPrisma";
+//import { MainPrisma } from "../lib/MainPrisma";
+import { db } from "../lib/MainDrizzle";
+//import { users } from "../db/schema";
 import { verifyPassword } from "../utils/auth";
 import { signToken } from "../utils/jwt";
 
 export const loginService = async (email: string, password: string) => {
-    const user = await MainPrisma.users.findUnique({
-        where: { email },
+    const user = await db.query.users.findFirst({
+        where: (users, {eq}) => eq(users.email, email)
     });
 
     if (!user) {
@@ -12,7 +14,7 @@ export const loginService = async (email: string, password: string) => {
     }
 
     const isValid = await verifyPassword(
-        password, user.salt, user.password);
+        password, user.salt, user.hashedpassword);
     if (!isValid) {
         throw new Error("INVALID_CREDENTIALS");
     }
