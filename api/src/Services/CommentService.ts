0e01396dedb035340a33f0e64be7053fe5c59dbd
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-01-25 18:56:51 +0100

    api - replaced all the Prisma Comment CRUD logic with Drizzle

--- api/src/Services/CommentService.ts
+++ api/src/Services/CommentService.ts
@@ -1,28 +1,44 @@
-import { MainPrisma } from "../lib/MainPrisma";
-import { CommentCreateSchema, CommentResponseSchema, 
-    CommentCreateDTO, CommentUpdateDTO } from "../schemas/CommentSchema";
+//import { MainPrisma } from "../lib/MainPrisma";
+import { db } from "../lib/MainDrizzle";
+import { comments} from "../db/schema";
+import { eq } from "drizzle-orm";
+import { CommentCreateSchema, CommentResponseSchema, CommentCreateDTO, CommentUpdateDTO } from "../schemas/CommentSchema";
+
+import { createInsertSchema, createSelectSchema, createUpdateSchema } from 'drizzle-zod';
+
+const selectSchema = createSelectSchema(comments);
+const selectFliterSchema = createSelectSchema(comments).pick({fkProductId: true});
+const deleteFliterSchema = createSelectSchema(comments).pick({id: true});
+const addSchema = createInsertSchema(comments);
+const updateSchema = createUpdateSchema(comments);
+
 
 export const createComment = async (input: CommentCreateDTO) => {
-    const newComment = await MainPrisma.comments.create({data: input});
-    return CommentCreateSchema.parse(newComment);
+    const newComment = addSchema.parse(input);
+    return await db.insert(comments).values(newComment);
 };
 
 export const getComments = async () => {
-    const Comments = await MainPrisma.comments.findMany();
-    return Comments.map((u) =>  CommentResponseSchema.parse(u));
+    const Comments = await db.select().from(comments);
+    return selectSchema.array().parse(Comments);
 };
 
-export const getCommentById = async (id: number) => {
-    const Comment = await MainPrisma.comments.findUnique({ where: { id } });
-    return Comment ? CommentResponseSchema.parse(Comment) : null;
+export const getCommentById = async (fk_Pro_Id: number) => {
+    const validFkProductId  = selectFliterSchema.parse(fk_Pro_Id);
+    const rows = await db.select().from(comments).where(eq(comments.fkProductId, validFkProductId.fkProductId));
+
+    return selectSchema.array().parse(rows);
 };
 
 export const updateComment = async (id: number, data: CommentUpdateDTO) => {
-    const updatedComment = await MainPrisma.comments.update({ where: { id }, data });
-    return CommentResponseSchema.parse(updatedComment)
+    const updatedComment = updateSchema.parse(data);
+    const newComment = await db.update(comments).set(updatedComment).where(eq(comments.id, id))
+    return selectSchema.parse(newComment);
 };
 
 export const deleteComment = async (id: number) => {
-    const deletedComment = await MainPrisma.comments.delete({ where: { id } });
-    return CommentResponseSchema.parse(deletedComment);
+    const deletedComment = deleteFliterSchema.parse(id);
+    const result = await db.delete(comments).where(eq(comments.id, deletedComment.id));
+    
+    return result;
 }
