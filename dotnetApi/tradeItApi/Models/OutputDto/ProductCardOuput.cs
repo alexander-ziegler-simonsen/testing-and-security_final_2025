e185e5624a73672b109f36f858e6d016647b7e20
Alexander Ziegler <29373163+alexander-ziegler-simonsen@users.noreply.github.com>
2026-02-22 16:01:37 +0100

    api - clearnup

---
+++ dotnetApi/tradeItApi/Models/OutputDto/ProductCardOuput.cs
@@ -0,0 +1,12 @@
+﻿namespace tradeItApi.Models.OutputDto;
+
+public partial class ProductCardOuput
+{
+    public int id { get; set; }
+
+    public string title { get; set; } = null!;
+
+    public decimal price { get; set; }
+
+    public string? url { get; set; }
+}
